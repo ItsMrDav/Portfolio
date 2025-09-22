c753499348d843e54ad5b24ea8bf7abe6759506d
Davut Simsek <davutt.simsek@gmail.com>
2025-09-22 20:27:43 +0200

    feat: add custom Tailwind theme, animations, and utility classes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import NotFound from './pages/NotFound';
 
-function App() {
-  const [count, setCount] = useState(0);
-
-  return <>TEST</>;
+export default function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
-
-export default App;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+export default function Home() {
+  return (
+    <div className="min-h-screen">
+      {/* {The Toggle} */}
+
+      {/* {Background Effects} */}
+
+      {/* Navbar  */}
+
+      {/* Main Content  */}
+
+      {/* Footer  */}
+    </div>
+  );
+}
